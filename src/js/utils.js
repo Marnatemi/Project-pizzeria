0e0059ce2feff48b9e327c554c816efe4df6c325
Marlena Czerwińska <marlena.stef@gmail.com>
2019-09-07 02:16:44 +0200

    Split js files into components

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,4 +1,4 @@
-/* global Handlebars, dataSource */
+
 
 const utils = {}; // eslint-disable-line no-unused-vars
 
@@ -36,14 +36,14 @@ utils.serializeFormToObject = function(form){
   return output;
 };
 
-utils.convertDataSourceToDbJson = function(){
-  const productJson = [];
-  for(let key in dataSource.products){
-    productJson.push(Object.assign({id: key}, dataSource.products[key]));
-  }
+// utils.convertDataSourceToDbJson = function(){
+//   const productJson = [];
+//   for(let key in dataSource.products){
+//     productJson.push(Object.assign({id: key}, dataSource.products[key]));
+//   }
 
-  console.log(JSON.stringify({product: productJson, order: []}, null, '  '));
-};
+//   console.log(JSON.stringify({product: productJson, order: []}, null, '  '));
+// };
 
 Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
   return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
@@ -52,3 +52,5 @@ Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
 Handlebars.registerHelper('joinValues', function(input, options) {
   return Object.values(input).join(options.fn(this));
 });
+
+export default utils;
